e9fd3bb1da2f8048cdc0466faea1e19780884293
Barbara Pascal <pascalba92@gmail.com>
2025-02-02 09:08:18 +0100

    recursive dataset loading

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -46,11 +46,89 @@ def load_data(the_dataset,the_representation):
         
         d: human dissimilarity ratings stored in a l x (l-1)/2 vector
     """
+
+    if type(the_dataset) == list:
+
+        r,D,d = load_multiple_data(the_dataset,the_representation)
+
+    else:
+        
+        with open('data/'+the_representation+'/'+the_dataset+'.pkl','rb') as Sp:
+        
+            data = pkl.load(Sp)
+            
+        # representation
+        r = data['representations']
+        
+        # dissimilarity matrix
+        D = data['dissimilarities']
+        
+        # dissimilarity vector
+        P = np.size(D,0)
+        d = np.zeros(P*(P-1)//2)
+        l = 0
+        for i in range(P):
+            for j in range(i+1,P):
+                d[l] = D[i,j]
+                l += 1
+    
+    return r,D,d
+
+
+def load_multiple_data(the_datasets,the_representation):
+    
+    """
+    Load human dissimilarity ratings and the audio sample representations.
+    
+    Args:
+    
+        the_datasets: list of strings containing the name of the datasets among
+        - 'Grey1977'
+        - 'Grey1978'
+        - 'Iverson1993_Whole'
+        - 'Iverson1993_Onset'
+        - 'Iverson1993_Remainder'
+        - 'McAdams1995'
+        - 'Lakatos2000_Harm'
+        - 'Lakatos2000_Perc'
+        - 'Lakatos2000_Comb'
+        - 'Barthet2010'
+        - 'Patil2012_A3'
+        - 'Patil2012_DX4'
+        - 'Patil2012_GD4'
+        - 'Siedenburg2016_e2set1'
+        - 'Siedenburg2016_e2set2'
+        - 'Siedenburg2016_e2set3'
+        - 'Siedenburg2016_e3'
+
+        the_representation: string containing the name of the representation among
+        - 'strf'
+        - 'stft'
+        - 'spectrum' (cochlea in the companion paper)
+        - 'scattering'
+        - 'clap'
+        - 'encodec'
+        - 'mert'
+        - 'mertcat'
+    
+    
+    Returns:
+    
+        r: n-dimensional representations of the L sounds composing the datasets, matrix of size L x n
+        
+        D: human dissimilarity ratings stored in a L x L matrix
+        
+        d: human dissimilarity ratings stored in a L x (L-1)/2 vector
+    """
+
+    # load the first dataset for initialization
+
+    the_dataset = the_datasets[0]
     
     with open('data/'+the_representation+'/'+the_dataset+'.pkl','rb') as Sp:
-    
-        data = pkl.load(Sp)
         
+            data = pkl.load(Sp)
+            
     # representation
     r = data['representations']
     
@@ -64,8 +142,34 @@ def load_data(the_dataset,the_representation):
     for i in range(P):
         for j in range(i+1,P):
             d[l] = D[i,j]
+            l += 1    
+
+    # loop over the multiple datasets
+
+    for the_dataset in the_datasets[1:]:
+        
+        with open('data/'+the_representation+'/'+the_dataset+'.pkl','rb') as Sp:
+        
+            data = pkl.load(Sp)
+            
+        # representation
+        r  = np.concatenate((r,data['representations']),axis = 1)
+        
+        # dissimilarity matrix
+        L = np.size(D,0)
+        l = np.size(data['dissimilarities'],0)
+        M = np.concatenate((np.nan * np.ones((l,L)),data['dissimilarities']),axis = 1)
+        D = np.concatenate((D,np.nan * np.ones((L,l))),axis = 1)
+        D = np.concatenate((D,M),axis = 0)
+
+        
+    # dissimilarity vector
+    P = np.size(D,0)
+    d = np.zeros(P*(P-1)//2)
+    l = 0
+    for i in range(P):
+        for j in range(i+1,P):
+            d[l] = D[i,j]
             l += 1
     
     return r,D,d
-    
-    
--- src/pearson.py
+++ src/pearson.py
@@ -37,7 +37,47 @@ def logpearson(w,f,ld):
         for j in range(i+1,N):
             lk[l] = -sum(w**(-2)*(f[:,i]-f[:,j])**2)
             l += 1
-    lp = 2/(N*(N-1))*sum((lk - np.mean(lk))/np.std(lk) * ld)
+    lp = 2/(N*(N-1))*np.sum((lk - np.mean(lk))/np.std(lk) * ld)
+    
+    return lp
+
+
+def logpearson_nan(w,f,ld):
+    
+    """
+    Compute the opposite Pearson correlation between parametric distance and human dissimilarity ratings.
+    
+    Args:
+    
+        w: weights of the parametric distance, vector of length n
+        
+        f: n-dimensional representations of the l sounds composing the dataset, matrix of size l x n
+        
+        ld: human dissimilarity ratings stored in a l x (l-1)/2 vector
+    
+    
+    Returns:
+    
+        lp: opposite Pearson correlation, real number
+    
+        
+    """
+    
+    
+    # standardize the dissimiarity vector
+    ld = (ld - np.nanmean(ld))/np.nanstd(ld)
+    
+    # dimension of features and number of sounds
+    (D,N) = np.shape(f)
+    
+    # compute and store the log-kernel
+    lk = np.zeros(N*(N-1)//2)
+    l = 0
+    for i in range(N):
+        for j in range(i+1,N):
+            lk[l] = -sum(w**(-2)*(f[:,i]-f[:,j])**2)
+            l += 1
+    lp = 2/(N*(N-1))*np.nansum((lk - np.nanmean(lk))/np.nanstd(lk) * ld)
     
     return lp
 
@@ -98,6 +138,64 @@ def dlogpearson(w,f,ld):
     # derivative of the Pearson correlation
     lp  = 2/(N*(N-1))*sum((lk - np.mean(lk))/np.std(lk) * ld)
     dlp = 2/(N*(N-1))*np.dot(slk**(-1)*ld,dlk - dmlk) - 2/(N*(N-1))*sum((lk - mlk)*ld)*dslk/slk**2
+    
+    return lp, dlp, dlk
+
+
+
+
+def dlogpearson_nan(w,f,ld):
+    
+    """
+    Compute the gradient with respect to the weights of the opposite Pearson correlation between parametric distance and human dissimilarity ratings
+    
+    Args:
+    
+        w: weights of the parametric distance, vector of length n
+        
+        f: n-dimensional representations of the l sounds composing the dataset, matrix of size l x n
+        
+        ld: human dissimilarity ratings stored in a l x (l-1)/2 vector
+    
+    
+    Returns:
+    
+        lp: opposite Pearson correlation, real number
+        
+        dlp: gradient of the opposite Pearson correlation, vector of length n
+        
+        dlk: Jacobian of the pairwise Pearson correlations, matrix of size s x n with s = l x (l-1) / 2
+    
+        
+    """
+    
+    # standardize the dissimiarity vector
+    ld = (ld - np.nanmean(ld))/np.nanstd(ld)
+    
+    # dimension of features and number of sounds
+    (D,N) = np.shape(f)
+    
+    # derivative of the log-kernel w.r.t. the weights
+    lk  = np.zeros(N*(N-1)//2)
+    dlk = np.zeros((N*(N-1)//2,D))
+    l   = 0
+    for i in range(N):
+        for j in range(i+1,N):
+            lk[l]    = -sum(w**(-2)*(f[:,i]-f[:,j])**2)
+            dlk[l,:] = 2*w**(-3)*(f[:,i]-f[:,j])**2
+            l += 1
+            
+    # derivative of the mean of the log-kernel
+    mlk  = np.nanmean(lk)
+    dmlk = (N*(N-1)//2)**(-1)*np.nansum(dlk,0);
+    
+    # derivative of the standard deviation of the log-kernel
+    slk  = np.nanstd(lk)
+    dslk = slk**(-1)*(N*(N-1)//2)**(-1)*np.dot((lk-mlk).T,dlk-dmlk);
+    
+    # derivative of the Pearson correlation
+    lp  = 2/(N*(N-1))*np.nansum((lk - np.nanmean(lk))/np.nanstd(lk) * ld)
+    dlp = 2/(N*(N-1))*np.dot(slk**(-1)*ld[~np.isnan(ld)],dlk[~np.isnan(ld),:] - dmlk) - 2/(N*(N-1))*np.nansum((lk - mlk)*ld)*dslk/slk**2
     
     return lp, dlp, dlk
 
